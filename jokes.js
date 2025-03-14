9858a0b4cbccde36bb1feb337b1a1a3c2470bc0c
nicoallgood <99364654+nicoallgood@users.noreply.github.com>
2025-03-14 10:55:44 +0100

    Adding jokes as well

--- jokes.js
+++ jokes.js
@@ -1,8 +1,8 @@
 const jokeSlides = [
-        {
-          content: "Quelle est la différence entre un marin et un boucher?",
-          sidenote: "Demandez la réponse à votre artisan!",
-        },
+        // {
+        //   content: "Quelle est la différence entre un marin et un boucher?",
+        //   sidenote: "Demandez la réponse à votre artisan!",
+        // },
         {
           content: "Qui est l'ami du feu?",
           sidenote: "Indice: c'est un plat servi par votre boucher",
