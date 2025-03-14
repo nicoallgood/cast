6bbaeb46a9ebe48f0b9e1c5bb3afcef0c49d8eff
nicoallgood <99364654+nicoallgood@users.noreply.github.com>
2025-03-14 10:55:32 +0100

    v2 with better structure and rendering

---
+++ text.js
@@ -0,0 +1,14 @@
+const textSlides = [
+        {
+          title: "À déguster cette semaine",
+          content:
+            "Nos tripes cuisinées, lasagnes, ou encore la tête de veau cuite",
+          sidenote: "",
+        },
+        {
+          title: "",
+          content:
+            "La viande fabrique de l'amitié, du respect. C'est un produit si noble qu'il génère comme aucun autre de la complicité et de l'humanité entre les hommes.",
+          sidenote: "",
+        },
+      ];
