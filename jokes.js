f4e2c89b78eedf27914c7ce3362f63c6c61ac979
nicoallgood <99364654+nicoallgood@users.noreply.github.com>
2025-03-25 09:07:41 +0100

    New jokes

--- jokes.js
+++ jokes.js
@@ -1,8 +1,16 @@
 const jokeSlides = [
         {
-          content: "Quelle est la différence entre un marin et un boucher?",
-          sidenote: "Demandez la réponse à votre artisan!",
+          content: "Qu'est-ce qui est rouge, qui ce mange, et qui n'est pas un steak?",
+          sidenote: "Indice: la réponse est dans la question!",
         },
+        // {
+        //   content: "Que fait une vache quand elle a les yeux fermés?",
+        //   sidenote: "Demandez la réponse à votre artisan!",
+        // },
+        // {
+        //   content: "Quelle est la différence entre un marin et un boucher?",
+        //   sidenote: "Demandez la réponse à votre artisan!",
+        // },
         // {
         //   content: "Qui est l'ami du feu?",
         //   sidenote: "Indice: c'est un plat servi par votre boucher",
