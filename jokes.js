949e012920853e46eb1557a5ef790cc723826c52
nicoallgood <99364654+nicoallgood@users.noreply.github.com>
2025-04-01 09:08:30 +0200

    New joke

--- jokes.js
+++ jokes.js
@@ -1,12 +1,12 @@
 const jokeSlides = [
-        {
-          content: "Qu'est-ce qui est rouge, qui ce mange, et qui n'est pas un steak?",
-          sidenote: "Indice: la réponse est dans la question!",
-        },
         // {
-        //   content: "Que fait une vache quand elle a les yeux fermés?",
-        //   sidenote: "Demandez la réponse à votre artisan!",
+        //   content: "Qu'est-ce qui est rouge, qui ce mange, et qui n'est pas un steak?",
+        //   sidenote: "Indice: la réponse est dans la question!",
         // },
+        {
+          content: "Que fait une vache quand elle a les yeux fermés?",
+          sidenote: "Demandez la réponse à votre artisan!",
+        },
         // {
         //   content: "Quelle est la différence entre un marin et un boucher?",
         //   sidenote: "Demandez la réponse à votre artisan!",
