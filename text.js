5188dc3e762e57c477b563aa27dca94c39b00e8b
nicoallgood <99364654+nicoallgood@users.noreply.github.com>
2025-06-07 11:05:41 +0200

    Text update

--- text.js
+++ text.js
@@ -5,16 +5,16 @@ const textSlides = [
             "Les Terrasses du Lac - Mazette! - L'Esquisse",
           sidenote: "Retrouvez notre viande au menu 🍽️",
         },
-        {
-         title: "Et les vainqueurs sont!",
-         content:
-           "Axelle et Paul, vous avez gagné le jeu du printemps!",
-         sidenote: "2 places pour les médiévales 🏆",
-        },
         // {
-        //   title: "",
-        //   content:
-        //     "La viande fabrique de l'amitié, du respect. C'est un produit si noble qu'il génère comme aucun autre de la complicité et de l'humanité entre les hommes.",
-        //   sidenote: "",
+        //  title: "Et les vainqueurs sont!",
+        //  content:
+        //    "Axelle et Paul, vous avez gagné le jeu du printemps!",
+        //  sidenote: "2 places pour les médiévales 🏆",
         // },
+        {
+          title: "",
+          content:
+            "La viande fabrique de l'amitié, du respect. C'est un produit si noble qu'il génère comme aucun autre de la complicité et de l'humanité entre les hommes.",
+          sidenote: "",
+        },
       ];
