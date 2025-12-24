ad23178c70f4e4c5e2dbf6f52de1df039a5d4dc7
nicoallgood <99364654+nicoallgood@users.noreply.github.com>
2025-12-24 05:30:25 +0100

    Update text.js

--- text.js
+++ text.js
@@ -14,7 +14,7 @@ const textSlides = [
         {
           title: "",
           content:
-            "La viande fabrique de l'amitié, du respect. C'est un produit si noble qu'il génère comme aucun autre de la complicité et de l'humanité entre les hommes.",
-          sidenote: "",
+            "Joyeuses Fêtes!",
+          sidenote: "et bon appétit!",
         },
       ];
